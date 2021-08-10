1258f7767c9ac469f839b36716e7f6938f3dc94f
Pickaxe828 <joshuajcn828@gmail.com>
2021-08-10 13:13:58 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#google sheet key: 13V9PNOB6ZfVIdO-G3XNnrgfQqB88P4OpObxQqomrf3k
+
+from instabot import Bot
+
+bot = Bot()
+
+
+
+bot.login(username="info.wtsmcsecret@gmail.com", password="ahPT~y?{@g:W8bBS")
